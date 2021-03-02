a356a03afaf830714bb14334b4480b862e095585
Lina155 <alinamackarova22@gmail.con>
2021-03-02 17:55:21 +0200

    Project

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 fetch('https://weather.com/weather/today/l/49.23,28.47?par=google&temp=c')
-.then((response) =>{
-    return response.json();
-})
-.then((data) =>{
-    console.log(data);
-});
+    .then(weather => {
+        console.log(weather);
+       // console.log(weather.text());
+       // weather.text().then(weather2 =>{
+        //    console.log(weather2);
+       // })
+        return weather.text();
+    })
+    .then(weather=> {
+        console.log(weather);
+    })
+
